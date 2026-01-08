907424c974a022af7403b2eb80ee7b833c3e3446
khaled11-ui <sramadan1@yahoo.com>
2026-01-08 22:46:08 +0000

    done

--- warmup/Main.java
+++ warmup/Main.java
@@ -3,7 +3,12 @@ package warmup;
 public class Main {
 
     public static void main(String[] args) {
-        
+        int sum = 0;
+        for (int i = 1; i <= 10; i++) {
+            if( i % 3 == 0 || i % 5 == 0 ) {
+                sum += i;
+            }
 
+    }    System.out.println("Sum of multiples of 3 or 5 between 1 and 1000: " + sum);
     }
 }
